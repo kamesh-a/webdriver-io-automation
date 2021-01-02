2119d2d5b594f545a37bcff354ebb4e9a6454bed
Kamesh <kamesh@anywhere.co>
2021-01-02 21:32:59 +0530

    generifying state initiation with given using regex

--- src/steps/given.ts
+++ src/steps/given.ts
@@ -3,6 +3,6 @@ import { Given } from "cucumber";
 /**
  * Reusing code based on regex parameters in other feature files.
  */
-Given(/^I open the url "([^"]*)?"$/, function (page: string): void {
-  browser.url(page);
+Given(/^I open the url "([^"]*)?"$/, function (urlToLoad: string): void {
+  browser.url(urlToLoad);
 });
