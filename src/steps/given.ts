7f1e92ed4e6b75c9642827d8f0f1d975b31bc25d
Kamesh <kamesh@anywhere.co>
2021-01-02 21:31:08 +0530

    Added comments

--- src/steps/given.ts
+++ src/steps/given.ts
@@ -1,9 +1,8 @@
 import { Given } from "cucumber";
 
-Given(
-  /^I open the (url|site) "([^"]*)?"$/,
-  function (type: "url" | "site", page: string): void {
-    const url = type === "url" ? page : browser.options.baseUrl + page;
-    browser.url(url);
-  }
-);
+/**
+ * Reusing code based on regex parameters in other feature files.
+ */
+Given(/^I open the url "([^"]*)?"$/, function (page: string): void {
+  browser.url(page);
+});
